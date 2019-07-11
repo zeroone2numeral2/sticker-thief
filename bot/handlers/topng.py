020aee6b202eb30286cc38e8de0a6a594c05518a
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-07-11 15:44:44 +0200

    Refactored png simple convertion

---
+++ bot/handlers/topng.py
@@ -0,0 +1,29 @@
+import logging
+
+# noinspection PyPackageRequirements
+from telegram.ext import MessageHandler, Filters
+# noinspection PyPackageRequirements
+from telegram import Update, ChatAction
+
+from bot import stickersbot
+from ..utils import decorators
+from bot.stickers import StickerFile
+
+logger = logging.getLogger(__name__)
+
+
+@decorators.restricted
+@decorators.action(ChatAction.UPLOAD_DOCUMENT)
+@decorators.failwithmessage
+def on_sticker_receive(update: Update, _):
+    logger.info('%d: user sent a stciker to convert', update.effective_user.id)
+
+    sticker = StickerFile(update.message.sticker)
+    sticker.download(prepare_png=True)
+
+    update.message.reply_document(sticker.png_bytes_object, quote=True)
+
+    sticker.delete()
+
+
+stickersbot.add_handler(MessageHandler(Filters.sticker, on_sticker_receive))
