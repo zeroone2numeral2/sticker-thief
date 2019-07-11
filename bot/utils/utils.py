93412057df081e010fecfed5f1b17ee73bd323cb
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-07-11 12:46:08 +0200

    Removed duplicate decorators in utils

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -1,11 +1,7 @@
 import logging
 import re
-from functools import wraps
-from html import escape as html_escape
 
 import emoji
-
-from config import config
 
 logger = logging.getLogger(__name__)
 
@@ -36,57 +32,3 @@ def name2link(name):
 
 def get_emojis(text):
     return ''.join(c for c in text if c in emoji.UNICODE_EMOJI)
-
-
-def action(chat_action):
-    def real_decorator(func):
-        @wraps(func)
-        def wrapped(update, context, *args, **kwargs):
-            context.bot.send_chat_action(update.effective_chat.id, chat_action)
-            return func(update, context, *args, **kwargs)
-
-        return wrapped
-
-    return real_decorator
-
-
-def failwithmessage(func):
-    @wraps(func)
-    def wrapped(update, context, *args, **kwargs):
-        try:
-            return func(update, context, *args, **kwargs)
-        except Exception as e:
-            logger.error('error while running handler callback: %s', str(e), exc_info=True)
-            text = 'An error occurred while processing the message: <code>{}</code>'.format(html_escape(str(e)))
-            if update.callback_query:
-                update.callback_query.message.reply_html(text)
-            else:
-                update.message.reply_html(text)
-
-    return wrapped
-
-
-def restricted(func):
-    @wraps(func)
-    def wrapped(update, context, *args, **kwargs):
-        user_id = update.effective_user.id
-        if config.telegram.admins_only and user_id not in config.telegram.admins:
-            logger.info('unauthorized access denied for %d', user_id)
-            update.message.reply_text('You are not allowed to use this bot')
-            return
-        return func(update, context, *args, **kwargs)
-
-    return wrapped
-
-
-def adminsonly(func):
-    @wraps(func)
-    def wrapped(update, context, *args, **kwargs):
-        user_id = update.effective_user.id
-        if user_id not in config.telegram.admins:
-            logger.warning('user %d tried to use a restriced command', user_id)
-            update.message.reply_text('You are not allowed to use this command')
-            return
-        return func(update, context, *args, **kwargs)
-
-    return wrapped
