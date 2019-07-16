1801199f9706c37764123ab97a054277b9a1bb64
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-07-16 15:24:00 +0200

    Updated /forgetme too

---
+++ bot/handlers/forgetme.py
@@ -0,0 +1,28 @@
+import logging
+
+# noinspection PyPackageRequirements
+from telegram.ext import CommandHandler
+# noinspection PyPackageRequirements
+from telegram import ChatAction, Update
+
+from bot import stickersbot
+from bot import strings as s
+from bot.utils import decorators
+from bot import db
+
+logger = logging.getLogger(__name__)
+
+
+@decorators.action(ChatAction.TYPING)
+@decorators.restricted
+@decorators.failwithmessage
+def on_forgetme_command(update: Update, _):
+    logger.info('%d: /forgetme', update.effective_user.id)
+
+    deleted_rows = db.delete_user_packs(update.effective_user.id)
+    logger.info('deleted rows: %d', deleted_rows or 0)
+
+    update.message.reply_text(s.FORGETME_SUCCESS)
+
+
+stickersbot.add_handler(CommandHandler(['forgetme', 'fm'], on_forgetme_command))
