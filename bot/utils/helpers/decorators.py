95acf45bbf82ef54039f4d38c344019979db96a5
zeroone2numeral2 <numeralzeroone@gmail.com>
2020-04-01 12:35:52 +0200

    Added ability to set maintenance mode

--- bot/utils/helpers/decorators.py
+++ bot/utils/helpers/decorators.py
@@ -92,9 +92,14 @@ def restricted(func):
     @wraps(func)
     def wrapped(update: Update, context: CallbackContext, *args, **kwargs):
         user_id = update.effective_user.id
-        if config.telegram.admins_only and user_id not in config.telegram.admins:
+        if (config.telegram.admins_only or config.telegram.get('maintenance_mode', False)) and user_id not in config.telegram.admins:
             logger.info('unauthorized access denied for %d', user_id)
-            update.message.reply_text('You are not allowed to use this bot')
+
+            if config.telegram.admins_only:
+                update.message.reply_text('You are not allowed to use this bot')
+            elif config.telegram.get('maintenance_mode', False):
+                update.message.reply_text("I'm sorry, the bot is undergoing some maintenance. Try again later!")
+
             return
         return func(update, context, *args, **kwargs)
 
