1ace1d02028454668e817fb726035ac875233137
zeroone2numeral2 <numeralzeroone@gmail.com>
2020-04-01 11:21:29 +0200

    Keep conversations status and temporary user data across restarts

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,10 +1,21 @@
+from telegram.ext import PicklePersistence
+
 from .utils import utils
 from .database import base
 from .bot import StickersBot
 from config import config
 
 
-stickersbot = StickersBot(token=config.telegram.token, use_context=True, workers=1)
+stickersbot = StickersBot(
+    token=config.telegram.token,
+    use_context=True,
+    persistence=PicklePersistence(
+        filename='persistence/data.pickle',
+        store_chat_data=False,
+        store_bot_data=False
+    ) if config.telegram.get('persistent_temp_data', True) else None,
+    workers=1
+)
 
 
 def main():
